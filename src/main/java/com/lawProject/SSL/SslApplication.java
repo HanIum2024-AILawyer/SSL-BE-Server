96b835bfbb55807eb4da851d61d4aac0e121790e
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2024-05-08 00:17:23 +0900

    Settings

---
+++ src/main/java/com/lawProject/SSL/SslApplication.java
@@ -0,0 +1,13 @@
+package com.lawProject.SSL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SslApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lawProject/SSL/SslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lawProject.SSL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
