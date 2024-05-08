b583b61bdb927b7b57bf2d9eb961c6ac037a7beb
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2024-05-09 02:05:07 +0900

    에러 공통 모듈 개발

---
+++ src/main/java/com/lawProject/SSL/common/Error/exception/BusinessException.java
@@ -0,0 +1,19 @@
+package com.lawProject.SSL.common.Error.exception;
+
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+
+    private ErrorCode errorCode;
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public BusinessException(String message, ErrorCode errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+}
