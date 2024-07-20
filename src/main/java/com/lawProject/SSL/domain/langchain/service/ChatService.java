bd574acfb919522e0dd02a55a8648d4cbe61a756
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2024-07-20 13:53:18 +0900

    feat: Message를 저장하는 로직 개발

---
+++ src/main/java/com/lawProject/SSL/domain/langchain/service/ChatService.java
@@ -0,0 +1,44 @@
+package com.lawProject.SSL.domain.langchain.service;
+
+import com.lawProject.SSL.domain.chatmessage.dao.MessageRepository;
+import com.lawProject.SSL.domain.chatmessage.model.ChatMessage;
+import com.lawProject.SSL.domain.chatroom.application.ChatRoomService;
+import com.lawProject.SSL.domain.chatroom.model.ChatRoom;
+import com.lawProject.SSL.domain.langchain.dto.MessageDto;
+import com.lawProject.SSL.domain.user.application.UserService;
+import com.lawProject.SSL.domain.user.model.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ChatService {
+    private final UserService userService;
+    private final ChatRoomService chatRoomService;
+    private final MessageRepository messageRepository;
+
+    public void saveMessage(MessageDto dto, String roomId) {
+        User user = userService.findById(dto.getSenderId());
+        ChatRoom chatRoom = chatRoomService.findByRoomId(roomId);
+
+        ChatMessage chatMessage = ChatMessage.builder()
+                .content(dto.getContent())
+                .sender(user)
+                .chatRoom(chatRoom)
+                .build();
+
+        messageRepository.save(chatMessage);
+        log.info("메시지 저장 완료");
+    }
+
+//    public Page<ChatMessage> getChatRoomMessages(String roomId, int page, int size) {
+//        ChatRoom chatRoom = chatRoomService.findByRoomId(roomId);
+//
+//        Pageable pageable = PageRequest.of(page-1, size, Sort.by("id").descending());
+//        Page<ChatMessage> messages = messageRepository.findByChatRoom(pageable, chatRoom);
+//
+//        return messages;
+//    }
+}
