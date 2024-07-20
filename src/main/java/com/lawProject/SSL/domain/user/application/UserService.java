993eff99a031a9f0d5a744b6f2ce1c55cb2310e9
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2024-07-20 15:08:38 +0900

    feat: User 객체를 편하게 가져오는 메서드 추가

--- src/main/java/com/lawProject/SSL/domain/user/application/UserService.java
+++ src/main/java/com/lawProject/SSL/domain/user/application/UserService.java
@@ -4,6 +4,8 @@ import com.lawProject.SSL.domain.user.dao.UserRepository;
 import com.lawProject.SSL.domain.user.exception.UserException;
 import com.lawProject.SSL.domain.user.model.User;
 import com.lawProject.SSL.global.common.code.ErrorCode;
+import com.lawProject.SSL.global.util.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
 
     public User findById(Long id) {
         return userRepository.findById(id).orElseThrow(
@@ -22,5 +25,9 @@ public class UserService {
         return userRepository.findByUserId(userId).orElseThrow(
                 () -> new UserException(ErrorCode.USER_NOT_FOUND)
         );
+    }
+
+    public User getUserInfo(HttpServletRequest request) {
+        return jwtUtil.getUserFromRequest(request);
     }
 }
