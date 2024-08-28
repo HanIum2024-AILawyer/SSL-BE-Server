7745df12cc9abfbbb88610bd398b2810b815ea4d
Youngjin <yyoungjin1229@gmail.com>
2024-08-28 15:21:01 +0900

    like 검색을 사용하기 위해 contains사용

--- src/main/java/com/lawProject/SSL/domain/lawyer/repository/LawyerRepository.java
+++ src/main/java/com/lawProject/SSL/domain/lawyer/repository/LawyerRepository.java
@@ -3,6 +3,8 @@ package com.lawProject.SSL.domain.lawyer.repository;
 import com.lawProject.SSL.domain.lawyer.model.Lawyer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 //repository, 인터페이스, JpaRepository 사용
 /**
@@ -16,6 +18,8 @@ public interface LawyerRepository extends JpaRepository<Lawyer, Long> {
     /* Id가 아닌 name으로 조회하고 싶다?
      * 일정한 규칙에 의해서 작성하면 됨*/
     Lawyer findByName(String name); // 이름으로 변호사 조회 기능 개발 끝!
+    /* like 검색을 쓰기위해서 사용*/
+    List<Lawyer> findByLawyerContains(String keyword);
 }
 
 //기본코드
