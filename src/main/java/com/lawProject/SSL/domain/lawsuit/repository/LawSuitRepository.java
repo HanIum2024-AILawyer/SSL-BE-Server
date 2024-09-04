8af4145c1dbb7f232f2411d174146823fbc153f3
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2024-09-04 23:20:26 +0900

    feat: 만료 기한(30일)이 지난 파일 자동 삭제 기능을 스케쥴러를 활용해 구현

--- src/main/java/com/lawProject/SSL/domain/lawsuit/repository/LawSuitRepository.java
+++ src/main/java/com/lawProject/SSL/domain/lawsuit/repository/LawSuitRepository.java
@@ -4,6 +4,7 @@ import com.lawProject.SSL.domain.lawsuit.model.LawSuit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,4 +13,7 @@ public interface LawSuitRepository extends JpaRepository<LawSuit, Long> {
     Optional<LawSuit> findByStoredFileName(String StoredFileName);
 
     List<LawSuit> findAllByUserIdOrderByIdDesc(Long userId);
+
+    // 만료 기한이 지난 LawSuit 찾기
+    List<LawSuit> findByExpireTimeBefore(LocalDateTime localDateTime);
 }
