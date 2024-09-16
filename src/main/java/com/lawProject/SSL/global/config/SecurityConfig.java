4ca0c8c076e188644712673a735bd0fac0cb9922
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2024-09-16 17:15:03 +0900

    fix: Cookie 값에는 공백 포함 불가 -> 토큰 앞에 Bearer를 붙여서 보안을 강화하던 방식을 제거

--- src/main/java/com/lawProject/SSL/global/config/SecurityConfig.java
+++ src/main/java/com/lawProject/SSL/global/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.lawProject.SSL.global.config;
 
-import com.lawProject.SSL.domain.token.repository.TokenRepository;
-import com.lawProject.SSL.domain.user.repository.UserRepository;
 import com.lawProject.SSL.global.oauth.handler.OAuthLoginFailureHandler;
 import com.lawProject.SSL.global.oauth.handler.OAuthLoginSuccessHandler;
 import com.lawProject.SSL.global.oauth.handler.OAuthLogoutHandler;
@@ -31,8 +29,6 @@ import java.util.Collections;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    private final UserRepository userRepository;
-    private final TokenRepository tokenRepository;
     private final JwtUtil jwtService;
     private final OAuthLoginSuccessHandler oAuthLoginSuccessHandler;
     private final OAuthLoginFailureHandler oAuthLoginFailureHandler;
@@ -83,7 +79,7 @@ public class SecurityConfig {
     @Bean
     public JwtAuthenticationProcessingFilter jwtAuthenticationProcessingFilter(){
 
-        return new JwtAuthenticationProcessingFilter(jwtService, userRepository);
+        return new JwtAuthenticationProcessingFilter(jwtService);
     }
 
     @Bean
--- src/main/java/com/lawProject/SSL/global/util/JwtUtilImpl.java
+++ src/main/java/com/lawProject/SSL/global/util/JwtUtilImpl.java
@@ -5,7 +5,6 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.lawProject.SSL.domain.token.exception.TokenException;
 import com.lawProject.SSL.domain.token.model.Token;
 import com.lawProject.SSL.domain.token.repository.BlacklistedTokenRepository;
-import com.lawProject.SSL.domain.token.repository.TokenRepository;
 import com.lawProject.SSL.domain.token.service.TokenService;
 import com.lawProject.SSL.domain.user.exception.UserException;
 import com.lawProject.SSL.domain.user.model.User;
@@ -51,7 +50,6 @@ public class JwtUtilImpl implements JwtUtil {
 
     private final UserRepository userRepository;
     private final BlacklistedTokenRepository blacklistedTokenRepository;
-    private final TokenRepository tokenRepository;
     private final TokenService tokenService;
 
     @Override
@@ -128,9 +126,7 @@ public class JwtUtilImpl implements JwtUtil {
             }
         }
 
-        return Optional.ofNullable(token).filter(
-                accessToken -> accessToken.startsWith(BEARER) //토큰이 Bearer로 시작하는지 확인
-        ).map(accessToken -> accessToken.substring(BEARER.length()).trim()); // Bearer 접두사 제거 후 공백 제거
+        return Optional.ofNullable(token);
     }
 
     @Override
