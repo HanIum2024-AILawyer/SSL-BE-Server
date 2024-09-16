6c2b7646f96ab73245445e6642b3344c0e2eb96e
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2024-09-16 17:56:22 +0900

    refactor: token의 전달/반환은 cookie로만 할 예정이기 때문에 기존 토큰 재발급 로직을 이에 맞춰 리팩토링

--- src/main/java/com/lawProject/SSL/global/config/SecurityConfig.java
+++ src/main/java/com/lawProject/SSL/global/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.lawProject.SSL.global.config;
 
+import com.lawProject.SSL.domain.token.repository.TokenRepository;
 import com.lawProject.SSL.global.oauth.handler.OAuthLoginFailureHandler;
 import com.lawProject.SSL.global.oauth.handler.OAuthLoginSuccessHandler;
 import com.lawProject.SSL.global.oauth.handler.OAuthLogoutHandler;
@@ -30,6 +31,7 @@ import java.util.Collections;
 @EnableWebSecurity
 public class SecurityConfig {
     private final JwtUtil jwtService;
+    private final TokenRepository tokenRepository;
     private final OAuthLoginSuccessHandler oAuthLoginSuccessHandler;
     private final OAuthLoginFailureHandler oAuthLoginFailureHandler;
     private final CustomOAuth2UserService customOAuth2UserService;
@@ -79,7 +81,7 @@ public class SecurityConfig {
     @Bean
     public JwtAuthenticationProcessingFilter jwtAuthenticationProcessingFilter(){
 
-        return new JwtAuthenticationProcessingFilter(jwtService);
+        return new JwtAuthenticationProcessingFilter(jwtService, tokenRepository);
     }
 
     @Bean
