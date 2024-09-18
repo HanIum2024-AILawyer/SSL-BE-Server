9bc243aba3deaa50d3fabe69c6b2a3912d36226c
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2024-09-18 16:55:25 +0900

    fix: Blacklist에 token을 저장할 때 token의 길이가 defalut 길이를 넘어서 발생하는 에러 해결

--- src/main/java/com/lawProject/SSL/domain/token/model/BlacklistedToken.java
+++ src/main/java/com/lawProject/SSL/domain/token/model/BlacklistedToken.java
@@ -1,5 +1,6 @@
 package com.lawProject.SSL.domain.token.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
@@ -12,5 +13,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class BlacklistedToken {
     @Id
+    @Column(name = "token", nullable = false, length = 500)
     private String token;
 }
